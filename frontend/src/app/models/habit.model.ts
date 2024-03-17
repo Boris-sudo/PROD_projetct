ff36beab1d6b6e09536b645b15afb46bfd6e9df6
boriskiva <boriskiva3@gmail.com>
2024-03-17 10:49:02 +0300

    changing HabitModel

--- frontend/src/app/models/habit.model.ts
+++ frontend/src/app/models/habit.model.ts
@@ -1,11 +1,9 @@
 export interface HabitModel {
-  id: number;
-
   title: string;
   period: 'daily' | 'weekly' | 'monthly';
   addDate: Date;
 
-  is_boolean: boolean;
+  type: 'checkbox' | 'value';
 
   targetValue?: number;
 }
