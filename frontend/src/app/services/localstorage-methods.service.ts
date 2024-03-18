e735a8d80b1591700044fe0d714650d6eff11294
boriskiva <boriskiva3@gmail.com>
2024-03-18 12:56:23 +0300

    adding toJson from string method

--- frontend/src/app/services/localstorage-methods.service.ts
+++ frontend/src/app/services/localstorage-methods.service.ts
@@ -1,36 +1,42 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LocalstorageMethodsService {
 
-  constructor() { }
+  constructor() {
+  }
 
   set(key: string, value: string) {
     localStorage.setItem(key, value);
   }
-  get(key:string) {
+
+  get(key: string) {
     let result = localStorage.getItem(key);
-    if (result==null) result='';
+    if (result == null) result = '';
     return result;
   }
 
   push(key: string, item: string) {
     let string_value = this.get(key);
-    if (string_value!='') string_value+=', ';
+    if (string_value != '') string_value += ', ';
     string_value += item;
     this.set(key, string_value);
   }
 
-  convertJsonToString(habit:any) {
+  convertJsonToString(habit: any) {
     let result = '{';
     for (const key in habit) {
       // @ts-ignore
       result += `"${key}": "${habit[key]}", `
     }
-    result=result.substring(0,result.length-2);
-    result+='}';
+    result = result.substring(0, result.length - 2);
+    result += '}';
     return result;
+  }
+
+  toJson(string_value: string): any {
+    return JSON.parse(string_value);
   }
 }
