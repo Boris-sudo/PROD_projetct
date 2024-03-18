48c85301669a13c7817399179acf335439f961d3
boriskiva <boriskiva3@gmail.com>
2024-03-18 11:29:03 +0300

    new Json methods with localstorage

--- frontend/src/app/services/localstorage-methods.service.ts
+++ frontend/src/app/services/localstorage-methods.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import {HabitModel} from "../models/habit.model";
 
 @Injectable({
   providedIn: 'root'
@@ -13,6 +14,24 @@ export class LocalstorageMethodsService {
   get(key:string) {
     let result = localStorage.getItem(key);
     if (result==null) result='';
+    return result;
+  }
+
+  push(key: string, item: string) {
+    let string_value = this.get(key);
+    if (string_value!='') string_value+=', ';
+    string_value += item;
+    this.set(key, string_value);
+  }
+
+  convertJsonToString(habit:any) {
+    let result = '{';
+    for (const key in habit) {
+      // @ts-ignore
+      result += `"${key}": "${habit[key]}", `
+    }
+    result=result.substring(0,result.length-2);
+    result+='}';
     return result;
   }
 }
