9920dd6c5e0d258a3a11facef63f897a3f81332d
boriskiva <boriskiva3@gmail.com>
2024-03-20 10:09:02 +0300

    editing login logic and adding login component

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -2,10 +2,14 @@ import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {MainPageComponent} from "./components/main-page/main-page.component";
 import {AddHabitPageComponent} from "./components/add-habit-page/add-habit-page.component";
+import {LoginComponent} from "./components/login/login.component";
 
 const routes: Routes = [
   {path: '', component: MainPageComponent},
   {path: 'habits', component: AddHabitPageComponent},
+  {path: 'explore', component: AddHabitPageComponent},
+  {path: 'profile', component: AddHabitPageComponent},
+  {path: 'login', component: LoginComponent},
 ];
 
 export interface NavBarLink {
@@ -14,6 +18,8 @@ export interface NavBarLink {
 }
 export const navBarLinks: NavBarLink[] = [
   {name: 'habits', url: 'habits'},
+  {name: 'explore', url: 'explore'},
+  {name: 'profile', url: 'profile'},
 ]
 
 @NgModule({
