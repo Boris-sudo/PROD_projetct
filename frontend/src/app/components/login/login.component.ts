597a826dcea79f7cd093fea60270960870477b5f
boriskiva <boriskiva3@gmail.com>
2024-03-20 10:39:20 +0300

    adding login functional

--- frontend/src/app/components/login/login.component.ts
+++ frontend/src/app/components/login/login.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {LocalstorageMethodsService} from "../../services/localstorage-methods.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-login',
@@ -6,10 +8,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  public username: string = '';
 
-  constructor() { }
+  constructor(
+    private localstorage: LocalstorageMethodsService,
+    private router: Router,
+  ) {
+  }
 
   ngOnInit(): void {
   }
 
+  login() {
+    this.localstorage.set('user', this.username);
+    this.router.navigate(['']);
+  }
 }
