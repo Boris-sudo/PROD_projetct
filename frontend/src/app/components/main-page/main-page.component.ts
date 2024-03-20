19fb397fd5ff06cf543dc5780531575973575790
boriskiva <boriskiva3@gmail.com>
2024-03-20 12:46:59 +0300

    changing main page

--- frontend/src/app/components/main-page/main-page.component.ts
+++ frontend/src/app/components/main-page/main-page.component.ts
@@ -1,41 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterViewInit, Component, OnInit} from '@angular/core';
 import {LocalstorageMethodsService} from "../../services/localstorage-methods.service";
 import {Router} from "@angular/router";
+import {CurrentDateService} from "../../services/current-date.service";
+
+
 
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
   styleUrls: ['./main-page.component.css']
 })
-export class MainPageComponent implements OnInit {
+export class MainPageComponent implements OnInit, AfterViewInit {
   public user: string = '';
 
   constructor(
+    public date_service: CurrentDateService,
     private localstorage: LocalstorageMethodsService,
     private router: Router,
   ) { }
 
   ngOnInit(): void {
+    // getting user
     this.user = this.localstorage.get('user');
     if (this.user == '')
       this.router.navigate(['login']).then();
   }
 
-
-
-  openMenu(id: string) {
-    const menu = document.getElementById(id)!;
-    menu.style.display = 'flex';
-    setTimeout(() => {
-      menu.classList.add('show');
-    })
-  }
-
-  closeMenu(id: string) {
-    const menu = document.getElementById(id)!;
-    menu.classList.remove('show');
-    setTimeout(() => {
-      menu.style.display = 'none';
-    }, 300);
+  ngAfterViewInit() {
   }
 }
