9aa65a91f2459f1c7b36a269eceace0ed10a3706
boriskiva <boriskiva3@gmail.com>
2024-03-21 21:52:59 +0300

    #11 changing top-bar right menu to be app-right-bar

--- frontend/src/app/components/top-bar/top-bar.component.ts
+++ frontend/src/app/components/top-bar/top-bar.component.ts
@@ -1,28 +1,19 @@
-import {AfterViewInit, Component, OnInit} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Router} from "@angular/router";
-import {CurrentDateService} from "../../services/current-date.service";
-import {navBarLinks} from "../../app-routing.module";
 
 @Component({
   selector: 'app-top-bar',
   templateUrl: './top-bar.component.html',
   styleUrls: ['./top-bar.component.css']
 })
-export class TopBarComponent implements OnInit, AfterViewInit {
-  public top_bar_links = navBarLinks;
+export class TopBarComponent implements OnInit {
 
   constructor(
     private router: Router,
-    private date_service: CurrentDateService,
   ) {
   }
 
   ngOnInit(): void {
-  }
-
-  ngAfterViewInit(): void {
-    // @ts-ignore
-    document.getElementById('current-data-input')!.value = this.date_service.get_date();
   }
 
   open_menu() {
@@ -54,22 +45,5 @@ export class TopBarComponent implements OnInit, AfterViewInit {
     this.close_menu();
   }
 
-  chose_date() {
-    const date_input = document.getElementById('current-data-input')!;
-    // @ts-ignore
-    const date = date_input.value;
-    if (date!='')
-      this.date_service.set_date(date);
-  }
-
-  next_day() {
-    this.date_service.next_day();
-    // @ts-ignore
-    document.getElementById('current-data-input')!.value = this.date_service.get_date();
-  }
-  prev_day() {
-    this.date_service.prev_day();
-    // @ts-ignore
-    document.getElementById('current-data-input')!.value = this.date_service.get_date();
-  }
+  protected readonly close = close;
 }
