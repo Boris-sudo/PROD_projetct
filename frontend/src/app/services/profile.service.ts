a8d0945a576ab33c3c5694d1c8d61fb9e6751eba
boriskiva <boriskiva3@gmail.com>
2024-03-22 10:21:39 +0300

    #11 new methods to profile_service

--- frontend/src/app/services/profile.service.ts
+++ frontend/src/app/services/profile.service.ts
@@ -1,5 +1,8 @@
 import {Injectable} from '@angular/core';
 import {LocalstorageMethodsService} from "./localstorage-methods.service";
+import {CurrentDateService} from "./current-date.service";
+import {HabitModel} from "../models/habit.model";
+import {DoneValueService} from "./done_value.service";
 
 export interface Profile {
   avatar_id?: number;
@@ -22,6 +25,8 @@ export class ProfileService {
 
   constructor(
     private localstorage: LocalstorageMethodsService,
+    private date_service: CurrentDateService,
+    private done_value_service: DoneValueService,
   ) {
   }
 
@@ -60,4 +65,56 @@ export class ProfileService {
     this.to_localstorage();
   }
 
+  add_daily_streak() {
+    this.from_localstorage();
+    this.profile.streak_days!++;
+    this.to_localstorage();
+  }
+
+  reset_daily_streak() {
+    this.from_localstorage();
+    this.profile.streak_days = 0;
+    this.to_localstorage();
+  }
+
+  calculate_level_xp() {
+
+  }
+
+  get_tasks(): HabitModel[] {
+    const date: string = this.date_service.get_date();
+    const string_value: string = this.localstorage.get('habits');
+    let all_habits: HabitModel[] = this.localstorage.toJson("[" + string_value + "]");
+    let habits: HabitModel[] = [];
+
+    for (const habit of all_habits) {
+      habit.addDate = new Date(habit.addDate);
+      if (habit.addDate.toJSON().split('T')[0] <= date)
+        habits.push(habit);
+    }
+    for (let i = 0; i < habits.length; i++) {
+      habits[i].doneValue = this.done_value_service.get(date, habits[i].id);
+      habits[i].targetValue = Number(habits[i].targetValue);
+    }
+
+    return habits;
+  }
+
+  count_done(): number {
+    const habits: HabitModel[] = this.get_tasks();
+    let result = 0;
+
+    for (const habit of habits)
+      if (habit.doneValue == habit.targetValue)
+        result++;
+
+    return result;
+  }
+
+  are_all_done(): boolean {
+    const habits: HabitModel[] = this.get_tasks();
+    const solved_count: number = this.count_done();
+
+    return habits.length == solved_count;
+  }
 }
