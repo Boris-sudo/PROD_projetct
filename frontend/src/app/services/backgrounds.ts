490424a6f5906078cdac6c13441e8e5f7d849552
boriskiva <boriskiva3@gmail.com>
2024-03-23 10:39:44 +0300

    #10 fixing shop and adding more backgrounds

--- frontend/src/app/services/backgrounds.ts
+++ frontend/src/app/services/backgrounds.ts
@@ -13,6 +13,20 @@ export class BackgroundService {
   private backgrounds: Background[] = [
     {cost: 0, color: '#d3ff00', edit_color: '#000000'},
     {cost: 0, color: '#3fff00', edit_color: '#000000'},
+    {cost: 100, color: '#00ffd0', edit_color: '#000000'},
+    {cost: 100, color: '#ffa600', edit_color: '#000000'},
+    {cost: 100, color: '#e600ff', edit_color: '#000000'},
+    {cost: 500, color: '#00b2ff', edit_color: '#000000'},
+    {cost: 500, color: '#ff0000', edit_color: '#000000'},
+    {cost: 500, color: '#0800ff', edit_color: '#000000'},
+    {cost: 1000, color: '#0009b2', edit_color: '#fff'},
+    {cost: 1000, color: '#ff6200', edit_color: '#000000'},
+    {cost: 1000, color: '#790bc0', edit_color: '#fff'},
+    {cost: 5000, color: '#3ea91a', edit_color: '#000000'},
+    {cost: 5000, color: '#d70637', edit_color: '#000000'},
+    {cost: 5000, color: '#4b4b4b', edit_color: '#fff'},
+    {cost: 10000, color: '#031259', edit_color: '#fff'},
+    {cost: 10000, color: '#131f24', edit_color: '#fff'},
   ];
 
   constructor() {
